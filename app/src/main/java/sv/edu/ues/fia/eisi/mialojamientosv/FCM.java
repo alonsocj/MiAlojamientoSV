3e6b5c0b406cbe2b09c41fd6b0fda378eef3cacc
williamVM4 <enriquemancia25@gmail.com>
2022-05-28 21:39:08 -0600

    notificaciones push

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/mialojamientosv/FCM.java
@@ -0,0 +1,70 @@
+package sv.edu.ues.fia.eisi.mialojamientosv;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class FCM extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.e("token","mi token "+token);
+        guardarToken(token);
+    }
+
+    private void guardarToken(String token) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("token");
+        ref.child("id").setValue(token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+        if (message.getData().size()>0){
+            String titulo = message.getData().get("Titulo");
+            String detalle = message.getData().get("detalle");
+            notificacion(titulo,detalle);
+        }
+    }
+    private void notificacion(String titulo,String detalle){
+        String id = "mensaje";
+        NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, id);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel nc = new NotificationChannel(id,"nuevo",NotificationManager.IMPORTANCE_HIGH);
+            nc.setShowBadge(true);
+            nm.createNotificationChannel(nc);
+        }
+        builder.setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle(titulo)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(detalle)
+                .setContentIntent(clicknoti())
+                .setContentInfo("nuevo");
+        Random random = new Random();
+        int idN = random.nextInt(8000);
+
+        assert nm != null;
+        nm.notify(idN,builder.build());
+    }
+
+    public PendingIntent clicknoti(){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        return PendingIntent.getActivity(this,0,intent,0);
+    }
+}
--- app/src/main/java/sv/edu/ues/fia/eisi/mialojamientosv/ui/view/FavoritosActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/mialojamientosv/ui/view/FavoritosActivity.java
@@ -8,10 +8,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import sv.edu.ues.fia.eisi.mialojamientosv.MainActivity;
 import sv.edu.ues.fia.eisi.mialojamientosv.R;
@@ -19,10 +37,11 @@ import sv.edu.ues.fia.eisi.mialojamientosv.databinding.ActivityFavoritosBinding;
 import sv.edu.ues.fia.eisi.mialojamientosv.databinding.ActivityMapaBinding;
 
 public class FavoritosActivity extends AppCompatActivity {
-
+    Button pushEspecifico;
     ActivityFavoritosBinding binding;
     BottomNavigationView navigationView;
     LottieAnimationView botonf;
+    DatabaseReference data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +49,19 @@ public class FavoritosActivity extends AppCompatActivity {
         binding = ActivityFavoritosBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
+        pushEspecifico = findViewById(R.id.pushE);
         navigationView = binding.bottomNavigation;
-
         navigationView.setSelectedItemId(R.id.favoritos);
         botonf = (LottieAnimationView) findViewById(R.id.animationFavorite);
+        data = FirebaseDatabase.getInstance().getReference();
+        consultaToken();
+        pushEspecifico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                consultaToken();
+            }
+        });
+
         botonf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,6 +78,50 @@ public class FavoritosActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void consultaToken(){
+        data.child("token").child("id").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String token = snapshot.getValue().toString();
+                    NotificacionEspecifico(token);
+                }else{
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void NotificacionEspecifico(String token) {
+        RequestQueue myrequest = Volley.newRequestQueue(getApplicationContext());
+        JSONObject json = new JSONObject();
+        try{
+            token ="eu61ErSkRQ2O8ybrE8Viqd:APA91bE8hZ-jipUWNzx5ZZtzonQnOQOO4ZB5tjtcvJ_6MqDgA49RQO-r6Pbfk3cdkrHyfR2W2NYWSMtNUW8KzjZzXUtjpTG4-hznl3oWrCHvHUY9JpJUSXwMTVXFy_zSEhQgHNt_GFsD";
+            json.put("to",token);
+            JSONObject notificacion =new JSONObject();
+            notificacion.put("titulo","Mensaje Nuevo");
+            notificacion.put("detalle","Tiene mensajes nuevos");
+            json.put("data",notificacion);
+            String url = "https://fcm.googleapis.com/fcm/send";
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,url,json,null,null){
+                @Override
+                public Map<String, String> getHeaders(){
+                    Map<String, String> header = new HashMap<>();
+                    header.put("Authorization", "key=AAAALA98pYw:APA91bHRnlGfODSkRge-a_oNmTdSWGLr1WlYytnXsr1FVTgdWHn_kjCL7Vmrv9OtgbEVDJozqAgKlKjyip2-h_3UEokH2VtLRPPnaffb0ZAjsCCkMZZESroqb5H3-fenUmzpQU-nzp3_");
+                    header.put("Content-type", "application/json");
+                    return header;
+                }
+            };
+            myrequest.add(request);
+        }catch (JSONException e){
+            e.printStackTrace();
+            Toast.makeText(this,"no sirve",Toast.LENGTH_LONG);
+        }
     }
 
     @SuppressLint("NonConstantResourceId")
